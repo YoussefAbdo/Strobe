fba3de9fb21098c6e304d3024d3d21eeb50a32c4
YoussefAbdo <mryoussefjoe@gmail.com>
2017-04-24 20:59:38 +0200

    1st meeting - Last step thresholding the Image
    1- imread
    2- cvtColor
    3- GaussianBlur
    4- Canny
    5- dilate
    6- erode
    7- findContours
    8- four_point_transform
    9- warpe
    10- thresh

---
+++ 1stTest.py
@@ -0,0 +1,316 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 23 11:47:52 2017
+
+@author: EngYo
+"""
+
+# import the necessary packages
+from __future__ import print_function
+from imutils.perspective import four_point_transform
+from imutils import contours
+from imutils import perspective
+import numpy as np
+import argparse
+import imutils
+import cv2
+from scipy.spatial import distance as dist
+
+def four_point_transform(image, pts):
+	# obtain a consistent order of the points and unpack them
+	# individually
+	rect = order_points(pts)
+	(tl, tr, br, bl) = rect
+ 
+	# compute the width of the new image, which will be the
+	# maximum distance between bottom-right and bottom-left
+	# x-coordiates or the top-right and top-left x-coordinates
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
+ 
+	# compute the height of the new image, which will be the
+	# maximum distance between the top-right and bottom-right
+	# y-coordinates or the top-left and bottom-left y-coordinates
+	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
+ 
+	# now that we have the dimensions of the new image, construct
+	# the set of destination points to obtain a "birds eye view",
+	# (i.e. top-down view) of the image, again specifying points
+	# in the top-left, top-right, bottom-right, and bottom-left
+	# order
+	dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
+ 
+	# compute the perspective transform matrix and then apply it
+	M = cv2.getPerspectiveTransform(rect, dst)
+	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+ 
+	# return the warped image
+	return warped
+
+def order_points_old(pts):
+	# initialize a list of coordinates that will be ordered
+	# such that the first entry in the list is the top-left,
+	# the second entry is the top-right, the third is the
+	# bottom-right, and the fourth is the bottom-left
+	rect = np.zeros((4, 2), dtype="float32")
+ 
+	# the top-left point will have the smallest sum, whereas
+	# the bottom-right point will have the largest sum
+	s = pts.sum(axis=1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+ 
+	# now, compute the difference between the points, the
+	# top-right point will have the smallest difference,
+	# whereas the bottom-left will have the largest difference
+	diff = np.diff(pts, axis=1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+ 
+	# return the ordered coordinates
+	return rect
+
+def order_points(pts):
+	# sort the points based on their x-coordinates
+	xSorted = pts[np.argsort(pts[:, 0]), :]
+ 
+	# grab the left-most and right-most points from the sorted
+	# x-roodinate points
+	leftMost = xSorted[:2, :]
+	rightMost = xSorted[2:, :]
+ 
+	# now, sort the left-most coordinates according to their
+	# y-coordinates so we can grab the top-left and bottom-left
+	# points, respectively
+	leftMost = leftMost[np.argsort(leftMost[:, 1]), :]
+	(tl, bl) = leftMost
+ 
+	# now that we have the top-left coordinate, use it as an
+	# anchor to calculate the Euclidean distance between the
+	# top-left and right-most points; by the Pythagorean
+	# theorem, the point with the largest distance will be
+	# our bottom-right point
+	D = dist.cdist(tl[np.newaxis], rightMost, "euclidean")[0]
+	(br, tr) = rightMost[np.argsort(D)[::-1], :]
+ 
+	# return the coordinates in top-left, top-right,
+	# bottom-right, and bottom-left order
+	return np.array([tl, tr, br, bl], dtype="float32")
+ 
+    
+
+
+
+
+# load our input image, convert it to grayscale, and blur it slightly
+image = cv2.imread("S_1_hppscan12.png")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred  = cv2.GaussianBlur(gray, (7, 7), 0)
+ 
+# perform edge detection, then perform a dilation + erosion to
+# close gaps in between object edges
+edged = cv2.Canny(blurred , 50, 100)
+edged = cv2.dilate(edged, None, iterations=1)
+edged = cv2.erode(edged, None, iterations=1)  
+  
+#cv2.imshow("edged", edged)
+
+# find contours in the edge map
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+ 
+# sort the contours from left-to-right and initialize the bounding box
+# point colors
+(cnts, _) = contours.sort_contours(cnts)
+colors = ((0, 0, 255), (240, 0, 159), (255, 0, 0), (255, 255, 0))
+
+# loop over the contours individually
+for (i, c) in enumerate(cnts):
+	# if the contour is not sufficiently large, ignore it
+	if cv2.contourArea(c) < 100000:
+		continue
+ 
+	# compute the rotated bounding box of the contour, then
+	# draw the contours
+	box = cv2.minAreaRect(c)
+	box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+	box = np.array(box, dtype="int")
+	cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
+ 
+	# show the original coordinates
+	print("Object #{}:".format(i + 1))
+	print(box) 
+    
+    # order the points in the contour such that they appear
+	# in top-left, top-right, bottom-right, and bottom-left
+	# order, then draw the outline of the rotated bounding
+	# box
+	rect = order_points_old(box)
+ 
+	# check to see if the new method should be used for
+	# ordering the coordinates
+	#if args["new"] > 0:
+		#rect = perspective.order_points(box)
+ 
+	# show the re-ordered coordinates
+	print(rect.astype("int"))
+	print("")
+    
+    	# loop over the original points and draw them
+	for ((x, y), color) in zip(rect, colors):
+		cv2.circle(image, (int(x), int(y)), 5, color, -1)
+ 
+	# draw the object num at the top-left corner
+	cv2.putText(image, "Object #{}".format(i + 1),
+		(int(rect[0][0] - 15), int(rect[0][1] - 15)),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.55, (255, 255, 255), 2)
+ 
+	# show the image
+#cv2.namedWindow( "Image", cv2.WINDOW_AUTOSIZE)
+cv2.imshow("Image", image)
+pts = np.array([[138, 1451],[117, 277],[1112, 259],[1133, 1433]], dtype = "float32")
+pts2 = np.array(box, dtype = "float32")
+warped = four_point_transform(gray, pts2)
+cv2.imshow("Warped", warped)
+
+# apply Otsu's thresholding method to binarize the warped
+# piece of paper
+thresh = cv2.threshold(warped, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+cv2.imshow("thresh", thresh)
+
+# find contours in the thresholded image, then initialize
+# the list of contours that correspond to questions
+cntsCircles = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cntsCircles = cntsCircles[0] if imutils.is_cv2() else cnts[1]
+questionCntsCircles = []
+
+# loop over the contours
+for c in cntsCircles:
+	# compute the bounding box of the contour, then use the
+	# bounding box to derive the aspect ratio
+	(x, y, w, h) = cv2.boundingRect(c)
+	ar = w / float(h)
+ 
+	# in order to label the contour as a question, region
+	# should be sufficiently wide, sufficiently tall, and
+	# have an aspect ratio approximately equal to 1
+	if w >= 20 and h >= 20 and ar >= 0.9 and ar <= 1.1:
+		questionCntsCircles.append(c)
+        
+
+
+
+cv2.waitKey(0)
+
+# construct the argument parse and parse the arguments
+#ap = argparse.ArgumentParser()
+#ap.add_argument("-i", "--image", required=True,
+#	help="path to the input image")
+#args = vars(ap.parse_args())
+
+# define the answer key which maps the question number
+# to the correct answer
+ANSWER_KEY = {0: 1,
+              1: 2,
+              2: 0,
+              3: 0,
+              4: 3,
+              5: 0,
+              6: 2,
+              7: 2,
+              8: 0,
+              9: 2,
+              10: 0,
+              11: 1,
+              12: 2,
+              13: 2,
+              14: 1,
+              15: 0,
+              16: 3,
+              17: 1,
+              18: 2,
+              19: 1,
+              20: 3,
+              21: 2,
+              22: 3,
+              23: 1,
+              24: 3,
+              25: 2,
+              26: 3,
+              27: 3,
+              28: 1,
+              29: 2,
+              30: 1,
+              31: 1,
+              32: 3,
+              33: 2,
+              34: 1,
+              35: 2,
+              36: 1,
+              37: 2,
+              38: 2,
+              39: 0,
+              40: 1,
+              41: 1,
+              42: 2,
+              43: 2,
+              44: 1}
+
+# load the image, convert it to grayscale, blur it
+# slightly, then find edges
+image = cv2.imread('S_1_hppscan12.png')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#cv2.imshow("gray", gray)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+#cv2.imshow("blurred", blurred)
+edged = cv2.Canny(blurred, 75, 200)
+#cv2.imshow("edged", edged)
+#cv2.imwrite('edged.png',edged)
+
+# find contours in the edge map, then initialize
+# the contour that corresponds to the document
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+docCnt = None
+
+# find contours in the edge map, then initialize
+# the contour that corresponds to the document
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+docCnt = None
+ 
+# ensure that at least one contour was found
+if len(cnts) > 0:
+	# sort the contours according to their size in
+	# descending order
+	cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+ 
+	# loop over the sorted contours
+	for c in cnts:
+		# approximate the contour
+		peri = cv2.arcLength(c, True)
+		approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+ 
+		# if our approximated contour has four points,
+		# then we can assume we have found the paper
+		if len(approx) == 4:
+			docCnt = approx
+			break
+        
+# apply a four point perspective transform to both the
+# original image and grayscale image to obtain a top-down
+# birds eye view of the paper
+paper = four_point_transform(image, docCnt.reshape(4, 2))
+warped = four_point_transform(gray, docCnt.reshape(4, 2))
+
+
+cv2.waitKey(0)
+
